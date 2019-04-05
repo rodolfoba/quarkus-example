aaa45c46ccef97681f8cf360285a17ec41a222f5
rodolfoba <rodolfoba87@gmail.com>
2019-04-04 21:56:35 -0300

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Quarkus example application
+[Quarkus](https://quarkus.io)
+
+* Quarkus native application
+* Prometheus
+* Grafana
+
+## Prerequisites
+* [Maven 3.5.3+](https://maven.apache.org/install.html)
+* [Java - OpenJDK 1.8+](https://adoptopenjdk.net/)
+
+## Running native-mode
+```bash
+$ mvn package -Pnative -Dnative-image.docker-build=true
+$ docker-compose up
+```
+
+* http://localhost:8080/hello or http://localhost:8080/hello/greeting/{name}
+* Grafana at: http://localhost:3000
+* Login as  admin/admin
+* Add datasource
+* Choose prometheus
+* URL = http://prom:9090
+* Click Save & Test
+* New Dashboard
+* Query = application:quarkus_example_greeting_resource_performed_checks
+
+
--- src/main/java/quarkus/example/GreetingResource.java
+++ src/main/java/quarkus/example/GreetingResource.java
@@ -20,14 +20,16 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    @Counted(name = "performedChecks", monotonic = true, description = "How many primality checks have been performed.")
-    @Timed(name = "checksTimer", description = "A measure how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
+    @Counted(name = "greetingChecks", monotonic = true, description = "How many greetings have been performed.")
+    @Timed(name = "greetingTimer", description = "A measure how long it takes to greet.", unit = MetricUnits.MILLISECONDS)
     public String greeting(@PathParam("name") String name) {
         return service.greeting(name);
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Counted(name = "helloChecks", monotonic = true, description = "How many helloes have been performed.")
+    @Timed(name = "helloTimer", description = "A measure how long it takes to say hello.", unit = MetricUnits.MILLISECONDS)
     public String hello() {
         return "hello";
     }
