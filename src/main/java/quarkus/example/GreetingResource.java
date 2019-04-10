f2be33921f67e3b610bd46575b4de74111e84705
rodolfoba <rodolfoba87@gmail.com>
2019-04-10 00:15:27 -0300

    config compose

--- src/main/java/quarkus/example/GreetingResource.java
+++ src/main/java/quarkus/example/GreetingResource.java
@@ -1,5 +1,9 @@
 package quarkus.example;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,8 +13,10 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metered;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
+@RequestScoped
 @Path("/hello")
 public class GreetingResource {
 
@@ -20,17 +26,20 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    @Counted(name = "greetingChecks", monotonic = true, description = "How many greetings have been performed.")
-    @Timed(name = "greetingTimer", description = "A measure how long it takes to greet.", unit = MetricUnits.MILLISECONDS)
+    @Counted(name = "greetingChecks", description = "How many greetings have been performed.", absolute = true)
+    @Timed(name = "greetingTimer", description = "A measure how long it takes to greet.", absolute = true, unit = MetricUnits.MILLISECONDS)
     public String greeting(@PathParam("name") String name) {
         return service.greeting(name);
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Counted(name = "helloChecks", monotonic = true, description = "How many helloes have been performed.")
-    @Timed(name = "helloTimer", description = "A measure how long it takes to say hello.", unit = MetricUnits.MILLISECONDS)
-    public String hello() {
+    @Counted(name = "helloChecks", description = "How many helloes have been performed.", absolute = true)
+    @Timed(name = "helloTimer", description = "A measure how long it takes to say hello.", absolute = true, unit = MetricUnits.MILLISECONDS)
+    @Metered(name = "helloMeter", absolute = true)
+    public String hello() throws Exception {
+        TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(5));
+//        TimeUnit.SECONDS.sleep(1);
         return "hello";
     }
 
