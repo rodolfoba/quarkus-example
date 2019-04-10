91a5e06885e6ffe30953566a84a8541d41ae93a8
rodolfoba <rodolfoba87@gmail.com>
2019-04-10 16:59:55 -0300

    Versioning grafana configs

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM maven:3.6-jdk-11-slim
-
-WORKDIR /app/
-COPY target/lib /app/lib
-COPY target/*-runner.jar /app/app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","app.jar"]
--- src/main/java/quarkus/example/GreetingResource.java
+++ src/main/java/quarkus/example/GreetingResource.java
@@ -1,5 +1,6 @@
 package quarkus.example;
 
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
@@ -11,6 +12,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Metered;
@@ -26,20 +29,26 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    @Counted(name = "greetingChecks", description = "How many greetings have been performed.", absolute = true)
-    @Timed(name = "greetingTimer", description = "A measure how long it takes to greet.", absolute = true, unit = MetricUnits.MILLISECONDS)
+//    @Counted(name = "greetingChecks", description = "How many greetings have been performed.", absolute = true)
+//    @Timed(name = "greetingTimer", description = "A measure how long it takes to greet.", absolute = true, unit = MetricUnits.MILLISECONDS)
     public String greeting(@PathParam("name") String name) {
         return service.greeting(name);
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Counted(name = "helloChecks", description = "How many helloes have been performed.", absolute = true)
-    @Timed(name = "helloTimer", description = "A measure how long it takes to say hello.", absolute = true, unit = MetricUnits.MILLISECONDS)
+    @Counted(name = "helloChecks", absolute = true)
+    @Timed(name = "helloTimer", absolute = true, unit = MetricUnits.MILLISECONDS)
     @Metered(name = "helloMeter", absolute = true)
+    @CircuitBreaker(delay = 1, delayUnit = ChronoUnit.SECONDS, failureRatio = 0.90, requestVolumeThreshold = 20)
+    @Timeout(unit = ChronoUnit.SECONDS, value = 5)
     public String hello() throws Exception {
+//        boolean falha = ThreadLocalRandom.current().nextBoolean();
+//        if (falha) {
+//            throw new RuntimeException();
+//        }
         TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(5));
-//        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(1);
         return "hello";
     }
 
